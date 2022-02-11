2a87f93341965d14eb69529acc00caeb7c49d868
ale-mazz <broodyblack@hotmail.it>
2022-02-11 18:22:07 +0100

    Cleanup

--- App.tsx
+++ App.tsx
@@ -1,78 +1,25 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Settings from "./components/settings";
-import Calendar from "./components/calendar";
-import Home from "./components/home";
 import { createStackNavigator } from "@react-navigation/stack";
 import { LogBox } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { useFonts } from "@expo-google-fonts/lato";
 import AppLoading from "expo-app-loading";
-import { Entypo, Feather } from "@expo/vector-icons";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux";
 import { MenuProvider } from "react-native-popup-menu";
 import { PersistGate } from "redux-persist/integration/react";
+import { TabNavigator } from "./components/navigator/tab-navigator";
+import { useFonts } from "expo-font";
 
 type StackParamList = {
   TabNavigator: undefined;
 };
-const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator<StackParamList>();
 
 LogBox.ignoreLogs([
   "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
 ]);
 
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarActiveTintColor: "cornflowerblue",
-        tabBarInactiveTintColor: "gray",
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarLabel: "Home",
-          tabBarShowLabel: false,
-          headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => (
-            <Entypo name="list" size={focused ? 36 : 24} color={color} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Calendar"
-        component={Calendar}
-        options={{
-          tabBarLabel: "Home",
-          tabBarShowLabel: false,
-          headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => (
-            <Entypo name="calendar" size={focused ? 36 : 24} color={color} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Settings"
-        component={Settings}
-        options={{
-          tabBarLabel: "Home",
-          tabBarShowLabel: false,
-          headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => (
-            <Feather name="settings" size={focused ? 36 : 24} color={color} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-};
-
-export default function App(): JSX.Element {
+const App = () => {
   let [fontsLoaded] = useFonts({
     "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.otf"),
     "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.otf"),
@@ -102,4 +49,6 @@ export default function App(): JSX.Element {
       </PersistGate>
     </Provider>
   );
-}
+};
+
+export default App;
---
+++ components/navigator/tab-navigator.tsx
@@ -0,0 +1,55 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../home";
+import { Entypo, Feather } from "@expo/vector-icons";
+import Calendar from "../calendar";
+import Settings from "../settings";
+
+const Tab = createBottomTabNavigator();
+
+export const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "cornflowerblue",
+        tabBarInactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => (
+            <Entypo name="list" size={focused ? 36 : 24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={Calendar}
+        options={{
+          tabBarLabel: "Home",
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => (
+            <Entypo name="calendar" size={focused ? 36 : 24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarLabel: "Home",
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => (
+            <Feather name="settings" size={focused ? 36 : 24} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
